56bc89636a324bb369b577e70f484daf38b57b82
GustavoSilverio <gustavosk290905@gmail.com>
2022-11-08 19:08:55 -0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './App.css';
+import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 
@@ -12,7 +13,7 @@ function App() {
       <div className="container">
         <Home />
       </div>
-
+      <Footer />
     </div>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,9 +4,9 @@ import './home.css';
 
 const Home = () => {
     return (
-
-        <h1>Home</h1>
-
+        <>
+            <h1>Home</h1>
+        </>
     );
 }
 
