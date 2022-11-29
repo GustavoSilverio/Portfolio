8de4bd95358057404063aaf2dfcdaebc160f12b2
GustavoSilverio <gustavosk290905@gmail.com>
2022-11-29 18:52:07 -0300

    tela-projetos

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 import './Projects.css';
 
 const Projects = () => {
     return (
-        <h1>Projects</h1>
+        <>
+            <div className="projetos">
+                <Card className='card' style={{ width: '18rem' }}>
+                    <Card.Img className='image' variant="top" src="https://via.placeholder.com/300x180" />
+                    <Card.Body className='texto' >
+                        <Card.Title className='title'>CARD TITLE</Card.Title>
+                        <Card.Text className='body-text'>
+                        Explicação sobre
+                        </Card.Text>
+                    </Card.Body>    
+                </Card>
+                <Card className='card' style={{ width: '18rem' }}>
+                    <Card.Img className='image' variant="top" src="https://via.placeholder.com/300x180" />
+                    <Card.Body className='texto' >
+                        <Card.Title className='title'>CARD TITLE</Card.Title>
+                        <Card.Text className='body-text'>
+                        Explicação sobre
+                        </Card.Text>
+                    </Card.Body>    
+                </Card>
+                <Card className='card' style={{ width: '18rem' }}>
+                    <Card.Img className='image' variant="top" src="https://via.placeholder.com/300x180" />
+                    <Card.Body className='texto' >
+                        <Card.Title className='title'>CARD TITLE</Card.Title>
+                        <Card.Text className='body-text'>
+                        Explicação sobre
+                        </Card.Text>
+                    </Card.Body>    
+                </Card>
+                <Card className='card' style={{ width: '18rem' }}>
+                    <Card.Img className='image' variant="top" src="https://via.placeholder.com/300x180" />
+                    <Card.Body className='texto' >
+                        <Card.Title className='title'>CARD TITLE</Card.Title>
+                        <Card.Text className='body-text'>
+                        Explicação sobre
+                        </Card.Text>
+                    </Card.Body>    
+                </Card>
+                <Card className='card' style={{ width: '18rem' }}>
+                    <Card.Img className='image' variant="top" src="https://via.placeholder.com/300x180" />
+                    <Card.Body className='texto' >
+                        <Card.Title className='title'>CARD TITLE</Card.Title>
+                        <Card.Text className='body-text'>
+                        Explicação sobre
+                        </Card.Text>
+                    </Card.Body>    
+                </Card>
+                <Card className='card' style={{ width: '18rem' }}>
+                    <Card.Img className='image' variant="top" src="https://via.placeholder.com/300x180" />
+                    <Card.Body className='texto' >
+                        <Card.Title className='title'>CARD TITLE</Card.Title>
+                        <Card.Text className='body-text'>
+                        Explicação sobre
+                        </Card.Text>
+                    </Card.Body>    
+                </Card>
+            </div>
+        </>
+
     );
 }
 
